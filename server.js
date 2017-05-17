4965bf1641360a32e5ba1ad95d2d081f06e1b4c2
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-05-17 11:00:38 -0400

    starting files

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var router = require('./controller/htmlRoutes.js');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+	type: "application/vnd.api+json"
+}));
