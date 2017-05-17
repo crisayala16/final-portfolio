4c0f060eba92e5acdc32b60067fc616287c7e837
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-05-17 14:59:44 -0400

    udpate

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var router = require('./controller/htmlRoutes.js');
 
 var app = express();
 var port = process.env.PORT || 8080;
@@ -14,3 +13,12 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({
 	type: "application/vnd.api+json"
 }));
+
+//Allows page access to external public files
+app.use(express.static(path.join(__dirname, '/views/public')));
+//html routes
+app.use(require('./controller/htmlRoutes.js'));
+
+app.listen(port, function(){
+	console.log('listening on port' + port);
+});
