04374910f1d37a6bf9eef7e65a9126ac0aa100b9
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-06-09 22:16:35 -0400

    update

--- views/public/js/logic.js
+++ views/public/js/logic.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var animateNav = false;
 	var portAnimation = function(){
 			$('div.port-item').delay(500).animate({
 				opacity: 1
@@ -33,14 +34,32 @@ $(document).ready(function(){
 			if($(this).height() < 1000){
 				if($(this).scrollTop() > 800 && $(this).scrollTop() < 1400){
 					$('#about-me-title').addClass('animated fadeInUp');
+
 				}
-				if($(this).scrollTop() > 1600){
+				if($(this).scrollTop() > 1600 && $(this).scrollTop() < 2000){
+					$('#skills-title').addClass('animated fadeInUp');
+				}
+				if($(this).scrollTop() > 2200){
 					$('#portfolio-title').addClass('animated fadeInUp');
 					portAnimation();
+				}
+				if($(this).scrollTop() > 0 && $(this).scrollTop() < 200){
+					$('.navi').css({background: 'rgba(48,62,78, 0)'});
+					$('.nav-item').css({
+						fontSize: '15px', 
+						color: '#fff'
+					});
+
+				}
+				if($(this).scrollTop() > 250 ){
+					$('.navi').css({background: 'rgba(48,62,78, 0.9)'});
+					$('.nav-item').css({
+						fontSize: '16px',
+						color: '#fff'
+					});
 				}
 			}
 		}
 	});
-
 
 })
