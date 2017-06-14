89ec23905632698484d8c96e886a0e33d4c380a1
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-06-14 17:54:25 -0400

    udpate

--- js/email.js
+++ js/email.js
@@ -4,12 +4,15 @@ myform.submit(function(event){
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
-  var template_id = "template_aBURJr3k";
+  var template_id = "portEmail";
 
   myform.find("button").text("Sending...");
   emailjs.sendForm(service_id,template_id,"myform")
     .then(function(){ 
       alert("Sent!");
+      $('#name-input').val('');
+      $('#email-input').val('');
+      $('#textArea').val('');
        myform.find("button").text("Send");
     }, function(err) {
        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
